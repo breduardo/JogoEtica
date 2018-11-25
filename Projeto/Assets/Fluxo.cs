d5cd1182224a875851aaf3018715d4019f1c41a3
Bruno <brunoed.0925@gmail.com>
2018-11-24 22:11:59 -0200

    ok

---
+++ Projeto/Assets/Fluxo.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Fluxo : MonoBehaviour
+{
+    public void CarregarFase1()
+    {
+        SceneManager.LoadScene("MainScene");
+    }
+}
